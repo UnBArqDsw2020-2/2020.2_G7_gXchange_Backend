0363ddbc3a53a614cb306660a4d6d1521df288a9
marcelog5 <marcelovictorg2@gmail.com>
2021-04-04 23:06:07 -0300

    Install and config django cors headers

--- api/settings.py
+++ api/settings.py
@@ -25,24 +25,31 @@ SECRET_KEY = "wnkd^*xm+8g7bq5z1ih--&f*xb^nngc8y)#v4eqsw*$17m@!3w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
 ALLOWED_HOSTS = []
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "app",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
