dd23405226a8eb2204e71ea978dc318a621054f4
igor-paiva <igorbatistapaiva@outlook.com>
2021-04-05 23:03:45 -0300

    Configure cors-headers in the application
    Co-authored-by: marcelog5 <marcelovictorg2@gmail.com>
    Co-authored-by: Rhuancpq <rhuancarlos.queiroz@gmail.com>

--- api/settings.py
+++ api/settings.py
@@ -25,14 +25,18 @@ SECRET_KEY = "wnkd^*xm+8g7bq5z1ih--&f*xb^nngc8y)#v4eqsw*$17m@!3w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
 
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -44,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
