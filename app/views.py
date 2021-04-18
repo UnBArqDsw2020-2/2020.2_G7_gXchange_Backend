994ae2c4d7b2f3ffb6ebab8a976c6235bd63ebbb
marcelog5 <marcelovictorg2@gmail.com>
2021-04-18 16:23:08 -0300

    Create update offer
    Co-authored-by: ThiagoGMF <thiago.guilherme20@hotmail.com>

--- app/views.py
+++ app/views.py
@@ -26,16 +26,16 @@ class CreateOffer(generics.CreateAPIView):
         # the back-end must find the logged user
         # leting this comment to make it easy for testing
 
-        # email = data.pop("user")
+        email = data.pop("user")
 
-        # person = Person.objects.filter(email=email)
+        person = Person.objects.filter(email=email)
 
-        # user = User.objects.filter(person=person[0])
+        user = User.objects.filter(person=person[0])
 
         pictures = data.pop("pictures")
 
         # something that rhuan tell
-        user = User.objects.all()
+        # user = User.objects.all()
 
         new_offer = Offer.objects.create(user=user[0], **data)
 
@@ -45,3 +45,29 @@ class CreateOffer(generics.CreateAPIView):
             Picture.objects.create(offer=new_offer, bin=binary_photo)
 
         return Response(status=201)
+
+class UpdateOffer(generics.RetrieveUpdateAPIView):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
+    lookup_field = "id"
+
+    def update(self, request, **kwargs):
+        data = request.data
+
+        pictures = data.pop("pictures")
+        
+        offer = Offer.objects.get(id=kwargs["id"])
+
+        Picture.objects.filter(offer_id=kwargs["id"]).delete()
+
+        for d in data:
+            offer.__dict__[d] = data[d]
+        
+        for picture in pictures:
+            binary_photo = base64ToBinary(picture["bin"])
+            Picture.objects.create(offer=offer, bin=binary_photo)
+        
+        offer.save()
+
+        return Response(status=201)
+    
